ea35c185378d91f0d47734c7533a906336228e2c
rockgustavo <grcuritiba@gmail.com>
2022-08-15 11:54:58 -0300

    15-08-2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDateComponent } from './components/parent-date/parent-date.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    ParentDateComponent
+    ParentDateComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule
