fdd66b0995e8cc1e84b023ec9a970d4432b35892
rockgustavo <grcuritiba@gmail.com>
2022-08-15 15:24:13 -0300

    15-08-2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDateComponent } from './components/parent-date/parent-date.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ParentDateComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    IfRenderComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule
